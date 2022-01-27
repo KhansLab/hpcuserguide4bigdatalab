0f3ea6a92fb31672baf09b3006febc9ce99ef09c
farukustunel <31048306+farukustunel@users.noreply.github.com>
2022-01-27 11:26:30 +0300

    changing conf.py for theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -30,7 +30,7 @@ release = '1.0'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [
+extensions = ["sphinx_theme"
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -47,7 +47,6 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-import sphinx_theme
 html_theme = 'stanford_theme'
 html_theme_path = [sphinx_theme.get_html_theme_path('stanford-theme')]
 # Add any paths that contain custom static files (such as style sheets) here,
